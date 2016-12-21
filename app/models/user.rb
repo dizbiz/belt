b5a76e4ed9089cf057ff61864f9c1178f9effd87
Daniel Burstyn <danielgburstyn@gmail.com>
2016-12-21 14:43:53 -0600

    final

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+ 	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+	validates :first_name, :last_name, presence: true, length: { in: 2..20 }
+	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+	before_save do
+		self.email.downcase!
+	end
+  before_create do
+    self.admin = false #assuming there is an admin field with a boolean value
+  end
+
+
   has_many :group_rosters, dependent: :destroy
   has_many :groups, through: :group_rosters, dependent: :destroy
   has_many :groups
